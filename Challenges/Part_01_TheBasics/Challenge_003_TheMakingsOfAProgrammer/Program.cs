e62825ffe14e659cf1f200c35d6e8f032e5349e6
WestvaleSec <zwc.cotten@gmail.com>
2025-10-15 20:24:47 -0500

    Completed Challenge 003 - The Makings of a Programmer

---
+++ Challenges/Part_01_TheBasics/Challenge_003_TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("This is a program.");
+Console.WriteLine("It doesn't do much, but it's a start.");
+Console.WriteLine("From here, you can build more complex programs.");
+Console.WriteLine("Happy coding!\n\n\n\n\n\n\n");
+Console.WriteLine("...\n\n\nactually, I needed one more WriteLine.");
